7eb7a7da6ced0273e32c023f89771e4065e6c5a4
Michael H Zhang <michaelhzhang@gmail.com>
2016-10-30 17:33:11 -0700

    Reverse traceroutes for route symmetry

--- traceroute.py
+++ traceroute.py
@@ -6,6 +6,7 @@ import re
 TIMESTAMP_PHRASE = "TIMESTAMP: "
 HOSTNAME_PHRASE = "ORIG_HOSTNAME: "
 TRACEROUTE_OUTPUT_PHRASE = "traceroute"
+REVERSE_PHRASE = "REVERSE"
 
 def run_traceroute(hostnames, num_packets, output_filename):
     curr_time = time.time()
@@ -29,6 +30,18 @@ def construct_command(hostname, num_packets):
 
 def parse_traceroute(raw_traceroute_filename, output_filename):
     trace_output = open(raw_traceroute_filename,"r").read().strip().split('\n')
+    if is_reverse(trace_output): # For route symmetry, output formats different
+        result = parse_reverse_traceroute(trace_output)
+    else:
+        result = parse_normal_traceroute(trace_output)
+    with open(output_filename,"a") as out: # Append instead of overwrite
+        json.dump(result, out)
+        out.write("\n") # Add newline
+
+def is_reverse(trace_output):
+    return ((len(trace_output)>0) and (trace_output[0] == REVERSE_PHRASE))
+
+def parse_normal_traceroute(trace_output):
     curr_hostname = None
     result = {}
     curr_trace = None
@@ -60,9 +73,7 @@ def parse_traceroute(raw_traceroute_filename, output_filename):
     if curr_hop is not None:
         curr_trace.append(curr_hop)
     record_results(curr_hostname, curr_trace, result)
-    with open(output_filename,"a") as out: # Append instead of overwrite
-        json.dump(result, out)
-        out.write("\n") # Add newline
+    return results
 
 def is_timestamp(line):
     length = len(TIMESTAMP_PHRASE)
@@ -130,6 +141,61 @@ def parse_hop(line):
         result["ip"] = ip
     return result
 
+def parse_reverse_traceroute(trace_output): # TODO
+    curr_hostname = None
+    result = {}
+    curr_trace = None
+    curr_hop = None
+    count = 0
+    for line in trace_output:
+        if is_timestamp(line):
+            result["timestamp"] = parse_timestamp(line)
+        elif is_hostname(line):
+            if curr_hostname is not None: # new trace
+                curr_trace.append(curr_hop)
+                record_results(curr_hostname, curr_trace, result)
+                curr_hop = None
+            curr_hostname = parse_hostname(line)
+            curr_trace = []
+        elif is_new_hop(line):
+            if curr_hop is not None:
+                curr_trace.append(curr_hop)
+            curr_hop = []
+            curr_hop.append(parse_reverse_hop(line))
+        elif is_continuing_reverse_hop(line):
+            curr_hop.append(parse_reverse_hop(line))
+        else: continue # ignore all other outputs
+    # Finish parsing last hop and trace
+    if curr_hop is not None:
+        curr_trace.append(curr_hop)
+    record_results(curr_hostname, curr_trace, result)
+    return result
+
+def is_continuing_reverse_hop(line):
+    tokens = line.split()
+    return ((len(tokens) >= 3) and is_AS(tokens[3]))
+
+def parse_reverse_hop(line):
+    if is_new_hop(line):
+        name_index = 1 # Is the name the 0th or 1st token?
+    else:
+        name_index = 0
+    tokens = line.split()
+    assert(len(tokens)>=3)
+    name = tokens[name_index]
+    ip = tokens[name_index+1].lstrip('(').rstrip(')')
+    ASN = tokens[name_index+2].lstrip('[AS').rstrip(']')
+    result = {}
+    if name == "*": # Not responding
+        result["asn"] = "None"
+        result["name"] = "None"
+        result["ip"] = "None"
+    else:
+        result["asn"] = ASN
+        result["name"] = name
+        result["ip"] = ip
+    return result
+
 def part_a_run(run_num):
     print("Running part a at time " + time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()))
     part_a_sites = ["google.com",
@@ -152,6 +218,9 @@ def part_b_run_from_our_computer():
                       "route-server.eastern.allstream.com"]
     run_traceroute(part_b_servers,5,"tmp_traces/tr_b_tmp")
     parse_traceroute("tmp_traces/tr_b_tmp","results/tr_b.json")
+
+def part_b_parse_reverse():
+    parse_traceroute("reverse_traces/combined-reverse-traces","results/tr_b.json")
 
 def main():
     # part_b_run_from_our_computer()
